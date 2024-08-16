330ae5796bac5e782be64cc28aac118df7a153f3
lterry <randoom97@live.com>
2024-08-16 15:22:31 -0600

    Handle concurrent clients

--- src/main/scala/codecrafters_redis/Server.scala
+++ src/main/scala/codecrafters_redis/Server.scala
@@ -3,12 +3,17 @@ package codecrafters_redis
 import codecrafters_redis.handlers.ServerHandler
 
 import java.net.{InetSocketAddress, ServerSocket}
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
 
 object Server {
   def main(args: Array[String]): Unit = {
     val serverSocket = new ServerSocket()
     serverSocket.bind(new InetSocketAddress("localhost", 6379))
-    val clientSocket = serverSocket.accept() // wait for client
-    ServerHandler.socketHandler(clientSocket)
+    while (true) {
+      val clientSocket = serverSocket.accept() // wait for client
+      Future(ServerHandler.socketHandler(clientSocket))
+    }
   }
 }
